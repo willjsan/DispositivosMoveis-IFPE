b2c1cdabf976cce28162c4d4d9a72dc726932eae
Willian jefferson dos Santos <willjsan@motorola.com>
2023-05-06 16:06:19 -0300

    Initializes empty A.S. Project
    [Desc.]
     - Initializes EmptyActivity default Android
    Studio project, builds end ren the application.

---
+++ app/src/main/java/recife/ifpe/edu/airpower/MainActivity.java
@@ -0,0 +1,14 @@
+package recife.ifpe.edu.airpower;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
