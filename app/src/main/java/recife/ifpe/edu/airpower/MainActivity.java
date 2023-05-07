06352597e8b35cc6439c2f63258d13ba49067c02
Willian jefferson dos Santos <willjsan@motorola.com>
2023-05-07 12:20:38 -0300

    Creates Main Activity layout
    [Desc.]
     - Creates the layout for Main Activity using a combination
    of view and viewGroup elements and a list view.
     - Creates the layout for list view item element

--- app/src/main/java/recife/ifpe/edu/airpower/MainActivity.java
+++ app/src/main/java/recife/ifpe/edu/airpower/MainActivity.java
@@ -1,8 +1,8 @@
 package recife.ifpe.edu.airpower;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
